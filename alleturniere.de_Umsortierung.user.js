fbb49f206f57cc44f7660d77568eac4a51cc24dc
Marcus Krassmann <marcus.krassmann@gmail.com>
2012-12-08 07:56:18 +0100

    Only try re-sort if match table has exactly 8 rows

--- alleturniere.de_Umsortierung.user.js
+++ alleturniere.de_Umsortierung.user.js
@@ -11,11 +11,13 @@ var tbody = document.querySelector("table.matches > tbody"),
     rows = document.querySelectorAll("table.matches > tbody > tr");
 
 // Umsortierung der 8 Spiele passend zum Victor-Spielbogen
-tbody.appendChild(rows[2]);
-tbody.appendChild(rows[0]);
-tbody.appendChild(rows[1]);
-tbody.appendChild(rows[7]);
-tbody.appendChild(rows[6]);
-tbody.appendChild(rows[5]);
-tbody.appendChild(rows[3]);
-tbody.appendChild(rows[4]);
+if (rows.length === 8) {
+    tbody.appendChild(rows[2]);
+    tbody.appendChild(rows[0]);
+    tbody.appendChild(rows[1]);
+    tbody.appendChild(rows[7]);
+    tbody.appendChild(rows[6]);
+    tbody.appendChild(rows[5]);
+    tbody.appendChild(rows[3]);
+    tbody.appendChild(rows[4]);
+}
