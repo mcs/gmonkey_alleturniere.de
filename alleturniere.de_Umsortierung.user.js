5cb3bf83851cc245e604076f8e7903c6938c4b40
Marcus Krassmann <marcus.krassmann@gmail.com>
2012-12-08 07:26:54 +0100

    First alpha that resorts results.
    I need to wait for the next match until I can complete the script to also
    work with the edit page.

---
+++ alleturniere.de_Umsortierung.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        alleturniere.de Umsortierung
+// @namespace   http://alleturniere.de/*
+// @description Sortiert Ergebniszeilen um
+// @include     http://alleturniere.de/sport/teammatch.aspx?id=*
+// @grant       none
+// @version     0.1
+// ==/UserScript==
+
+var tbody = document.querySelector("table.matches > tbody"),
+    rows = document.querySelectorAll("table.matches > tbody > tr");
+
+// Umsortierung der 8 Spiele passend zum Victor-Spielbogen
+tbody.appendChild(rows[2]);
+tbody.appendChild(rows[0]);
+tbody.appendChild(rows[1]);
+tbody.appendChild(rows[7]);
+tbody.appendChild(rows[6]);
+tbody.appendChild(rows[5]);
+tbody.appendChild(rows[3]);
+tbody.appendChild(rows[4]);
